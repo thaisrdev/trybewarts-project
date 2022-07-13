bdfa44da32fd3aed71ee364974bcc3f3a6c545db
Carolina Lima <carolina.jpeg@gmail.com>
2022-07-13 17:45:38 -0300

    req 13

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const botaoLogin = document.getElementById('botao-login');
-const email = document.getElementById('email');
-const senha = document.getElementById('senha');
-
-botaoLogin.addEventListener('click', verificacao);
+const botaoLogin = document.getElementById("botao-login");
+const email = document.getElementById("email");
+const senha = document.getElementById("senha");
 
 function verificacao() {
-    if (email.value.match('tryber@teste.com') && senha.value.match('123456')) {
-        alert('Olá, Tryber!');
-    } else {
-        alert('Email ou senha inválidos.');
-    }
-} 
+  if (email.value.match("tryber@teste.com") && senha.value.match("123456")) {
+    alert("Olá, Tryber!");
+  } else {
+    alert("Email ou senha inválidos.");
+  }
+}
+
+botaoLogin.addEventListener("click", verificacao);
