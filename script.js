708172974e389e21f3f28c3da3e12e4be0d58d6c
Thais Rosa <tsrosa18@gmail.com>
2022-07-13 16:07:43 -0300

    corrigindo requisito 3)

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const senha = document.getElementById('senha');
 botaoLogin.addEventListener('click', verificacao);
 
 function verificacao() {
-    if (email.value.match('tryber@teste.com') && senha.value.match('1234556')) {
+    if (email.value.match('tryber@teste.com') && senha.value.match('123456')) {
         alert('Olá, Tryber!');
     } else {
         alert('Email ou senha inválidos.');
