37e2f9daa5a76470c42c175d28597378d31cfbac
Thais Rosa <tsrosa18@gmail.com>
2022-07-13 18:13:01 -0300

    requisito 17

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ const botaoLogin = document.getElementById('botao-login');
 const email = document.getElementById('email');
 const senha = document.getElementById('senha');
 
-botaoLogin.addEventListener('click', verificacao);
-
 function verificacao() {
-    if (email.value.match('tryber@teste.com') && senha.value.match('123456')) {
-        alert('Olá, Tryber!');
-    } else {
-        alert('Email ou senha inválidos.');
-    }
-} 
+  if (email.value.match('tryber@teste.com') && senha.value.match('123456')) {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
+}
+
+botaoLogin.addEventListener('click', verificacao);
